09172d553ba2dbd773edcc1c1f03aad779c01625
Marcos Arias <marcosa@alberdisa.com.ar>
2026-06-06 09:01:45 -0300

    Agrega import redirect

--- farmacia_folder.py
+++ farmacia_folder.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import uuid
 from datetime import datetime
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from database.db import get_db_connection
 farmacia_folder_bp = Blueprint("farmacia_folder", __name__)
 
