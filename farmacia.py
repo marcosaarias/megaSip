887eee4c951717628651c962ba8d7f4d3dfff275
Marcos Arias <marcosa@alberdisa.com.ar>
2026-06-13 12:11:50 -0300

    agrega correcicion print

--- farmacia.py
+++ farmacia.py
@@ -237,10 +237,6 @@ def autocompletar_super_desde_cenefas(df):
     print("Columnas BD cenefas:", cenefas_df.columns.tolist(), flush=True)
 
     cenefas_df["Codigo_match"] = (
-
-        print(cenefas_df[["codigo", "Codigo_match"]].head(20).to_string(), flush=True)
-        print("UNIQUE Codigo_match:", cenefas_df["Codigo_match"].nunique(), flush=True)
-        
         cenefas_df["codigo"]
         .astype(str)
         .str.strip()
