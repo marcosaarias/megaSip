451ddbce2f5821fc2997e6965df4e262f4e92485
Marcos Arias <marcosa@alberdisa.com.ar>
2026-08-27 18:43:44 -0300

    agrega modificacion en total empresa mayorista

--- compras.py
+++ compras.py
@@ -24,7 +24,7 @@ from io import BytesIO
 
 SUCURSAL_MAP = {
     "Total Empresa": "CO01,CO02,CO04,CO05,CO06,CO07,CO08,CO09,CO10,CO11,CO12,CO14,CO15,CO16,CO17,CO18,CO19,CO20,CO21,CO22,CO23,CO24,CO25,CO26,CO27,CO28,CO29,MA02",
-    "Total Empresa - Mayorista": "CO05,CO09,CO12,CO15,CO21,CO24,CO25,CO29,MA02",
+    "Total Empresa - Mayorista": "CO05,CO09,CO12,CO15,CO21,CO29,MA02",
     "Jujuy - Mayorista": "CO05,CO12,CO15,MA02",
     "Salta - Mayorista": "CO09,CO29,CO21",
     "Oran - Mayorista": "CO21",
